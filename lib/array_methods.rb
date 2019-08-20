b206f3558f39dea3aaa535257d999946c27d6e14
Priscilla Firpo <priscillayf@gmail.com>
2019-08-20 21:02:43 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,15 +7,15 @@ def using_sort(array)
 end
 
 def using_reverse(array)
-
+  array.reverse
 end
 
 def using_first(array)
-
+  array.using_first
 end
 
 def using_last(array)
-
+  array.last
 end
 
 def using_size(array)
